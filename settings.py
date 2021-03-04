29cfe3fc7acece60c56ca7af13edefa752ec6954
Alexander Busse <ableph@yandex.com>
2021-03-04 01:15:25 +0100

    designyfy

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.178.28', '192.168.178.21', 'mondy.ableph.net',
-                 '127.0.0.1', 'localhost', '192.168.188.154']
+                 '127.0.0.1', 'localhost', '192.168.188.154', '192.168.1.137']
 
 
 # Application definition
