a96ac0852d35a1224442a0bdb98176b515b9bb16
Alexander Busse <ableph@yandex.com>
2021-03-04 18:58:24 +0100

    stuffystuf

--- settings.py
+++ settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.178.28', '192.168.178.21', 'mondy.ableph.net',
-                 '127.0.0.1', 'localhost', '192.168.188.154', '192.168.1.137']
+                 '127.0.0.1', 'localhost', '192.168.188.154', '192.168.1.137',
+                 'zirkusmond.de']
 
 
 # Application definition
