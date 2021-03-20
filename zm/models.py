e68e656936ed15e5c99c314e49e56437c5619873
Alexander Busse <ableph@yandex.com>
2021-03-20 03:40:25 +0100

    stream teaser, zm visitor

---
+++ zm/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Visitor(models.Model):
+    '''
+    '''
+    useragent = models.TextField()
+    ip = models.GenericIPAddressField()
+    referer = models.URLField()
