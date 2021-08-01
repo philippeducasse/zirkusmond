eabda0d36a8f1d48276780eeac91c80d1a3d9a6c
ableph <ableph@yandex.com>
2021-08-01 13:46:40 +0200

    fix deployment

--- start-server.sh
+++ start-server.sh
@@ -3,9 +3,10 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python /usr/src/manage.py createsuperuser --no-input)
 fi
-python /usr/src/manage.py collectstatic
+ln -s /usr/src/zirkusmond .
+yes |python /usr/src/manage.py collectstatic
 python /usr/src/manage.py makemigrations events
-python /usr/src/manage.py migrate
+#python /usr/src/manage.py migrate
 chown www-data:www-data -R .
 chown www-data:www-data db.sqlite3
 (gunicorn zirkusmond.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
