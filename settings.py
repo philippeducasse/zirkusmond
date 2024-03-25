55f6016acbba6f2ad50aa0b2f9a72093f2832775
Alexander Busse <alex@ableph.net>
2024-03-25 19:36:33 +0000

    email and requirement

--- settings.py
+++ settings.py
@@ -128,9 +128,12 @@ else:
 
 EMAIL_HOST_USER = 'reservation@zirkusmond.de'
 MAIL_HOST_CRED = os.environ.get("MAIL_HOST_CRED", "")
-EMAIL_HOST = '162.19.152.5'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'mail.ableph.net'
+#EMAIL_PORT = 465
+#EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#EMAIL_USE_SSL = True
 
 
 # Database
