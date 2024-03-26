dd4e7424fb81c56b9210d499292265b085534a40
Alexander Busse <alex@ableph.net>
2024-03-26 08:00:34 +0000

    automatic db migration

--- start-server.sh
+++ start-server.sh
@@ -6,7 +6,7 @@ fi
 ln -s /usr/src/zirkusmond .
 yes yes|python /usr/src/manage.py collectstatic
 python /usr/src/manage.py makemigrations events
-# python /usr/src/manage.py migrate
+python /usr/src/manage.py migrate events
 chown www-data:www-data -R .
 chown www-data:www-data db.sqlite3
 gunicorn zirkusmond.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3 &
