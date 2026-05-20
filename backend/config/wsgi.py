f1005e9661fd1e4c4443facbc91abb454489c6a3
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2026-05-20 14:45:20 +0200

    feat: allow django settings module configuration from environment

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
