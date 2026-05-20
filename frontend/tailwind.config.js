582591c22a48aac683c69bce063edbf4bc1194c1
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2026-05-20 13:19:50 +0200

    wip

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../zirkusmond_de/templates/**/*.{html,js}',
-    '../zirkusmond_de/events/templates/**/*.{html,js}',
+    '../backend/templates/**/*.{html,js}',
+    '../backend/events/templates/**/*.{html,js}',
     './src/components/**/*.vue',
   ],
   theme: {
