a9b72882af3f2cad0a095a2f6ed25a9c5f2cf7b5
Philippe Ducasse <ducassephi@hotmail.fr>
2026-05-22 15:32:11 +0200

    refactor: move Reservation FK in payment to reservations app

--- backend/events/models/__init__.py
+++ backend/events/models/__init__.py
@@ -1,11 +1,7 @@
 from .event import Event
 from .payment import ReservationPayment
-from .reservation import Guest, Person, Reservation
 
 __all__ = [
     "Event",
-    "Person",
-    "Reservation",
-    "Guest",
     "ReservationPayment",
 ]
