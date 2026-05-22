150867ce1f44e30c7905bb39ea605d7925ed2f51
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2026-05-22 12:41:56 +0200

    fix: correct related_name in all show managers

--- backend/shows/managers.py
+++ backend/shows/managers.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 
 class UpcomingShowManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(event__begin__gte=timezone.now()).distinct()
+        return super().get_queryset().filter(events__begin__gte=timezone.now()).distinct()
 
 
 class PastShowManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().exclude(event__begin__gte=timezone.now()).distinct()
+        return super().get_queryset().exclude(events__begin__gte=timezone.now()).distinct()
 
 
 class UnscheduledShowManager(models.Manager):
