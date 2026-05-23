fc5d976eeb7ab45f302064a990e8bb63a60cd5f9
Philippe Ducasse <ducassephi@hotmail.fr>
2026-05-23 21:11:14 +0200

    fix migration dep

--- backend/reservations/migrations/0004_migrate_payment_data.py
+++ backend/reservations/migrations/0004_migrate_payment_data.py
@@ -29,7 +29,7 @@ def copy_payments(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reservations', '0002_reservationpayment'),
+        ('reservations', '0003_alter_guest_ticket_id_nullable'),
     ]
 
     operations = [
