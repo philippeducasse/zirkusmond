a896fa71bcbef5953e5622d373eec4e4c4fb8192
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2026-05-27 12:47:39 +0200

    fix: custom stripe provider and payment env var mapping

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -28,7 +28,7 @@ PAYMENT_VARIANTS = {
         'endpoint': os.environ.get('PAYPAL_ENDPOINT', 'https://api.sandbox.paypal.com'),
         'capture': True,
     }),
-    'stripe': ('payments.stripe.providers.StripeProviderV3', {
+    'stripe': ('reservations.payments.stripe_provider.StripeProviderV3', {
         'api_key': os.environ.get('STRIPE_TEST_TOKEN', ''),
         'endpoint_secret': os.environ.get('STRIPE_HOOK_TOKEN', ''),
         'secure_endpoint': False,
