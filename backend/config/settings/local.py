30fed21d222b9a6928d85d42e3170ccb7bf4af86
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2026-05-27 12:22:43 +0200

    chore: cleanup and update configuration after module reorganization

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -23,14 +23,14 @@ PAYMENT_USES_SSL = False
 PAYMENT_VARIANTS = {
     'default': ('payments.dummy.DummyProvider', {}),
     'paypal': ('payments.paypal.PaypalProvider', {
-        'client_id': os.environ.get('PAYPAL_SANDBOX_CLIENT_ID', ''),
-        'secret': os.environ.get('PAYPAL_SANDBOX_SECRET', ''),
-        'endpoint': 'https://api.sandbox.paypal.com',
+        'client_id': os.environ.get('PAYPAL_CLIENT_ID', ''),
+        'secret': os.environ.get('PAYPAL_SECRET', ''),
+        'endpoint': os.environ.get('PAYPAL_ENDPOINT', 'https://api.sandbox.paypal.com'),
         'capture': True,
     }),
     'stripe': ('payments.stripe.providers.StripeProviderV3', {
         'api_key': os.environ.get('STRIPE_TEST_TOKEN', ''),
-        'endpoint_secret': os.environ.get('STRIPE_TEST_HOOK_TOKEN', ''),
+        'endpoint_secret': os.environ.get('STRIPE_HOOK_TOKEN', ''),
         'secure_endpoint': False,
     }),
 }
--- backend/views.py
+++ backend/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404, HttpResponse
 import datetime
 
-from shows.models import Show
-from rentals.models import RentalObject
+from django.shortcuts import redirect, render
 from newsletter.forms import NewsletterRegistrationForm
+from rentals.models import RentalObject
+from shows.models import Show
 
 
 def _upcoming_shows():
-    us = Show.objects.prefetch_related('events').all()
+    us = Show.objects.prefetch_related("events").all()
     us = list(filter(lambda x: x.show_in_preview(), us))
-    us = sorted(us, key=lambda x: datetime.date(2020, 1, 1) if not x.future_events() else x.future_events()[0].admission.date())
+    us = sorted(
+        us,
+        key=lambda x: datetime.date(2020, 1, 1)
+        if not x.future_events()
+        else x.future_events()[0].admission.date(),
+    )
     return us
+
 
 def _rental_objects():
     rental_objects = RentalObject.objects.all()
@@ -21,76 +26,105 @@ def _rental_objects():
 def plain(request):
     us = _upcoming_shows()[:6]
     newsletter_form = NewsletterRegistrationForm()
-    return render(request, 'index.html',
-                  {'upcoming_shows': us,
-                   'show_all_events_link': True,
-                   'show_home_link': False,
-                   'newsletter_form': newsletter_form})
+    return render(
+        request,
+        "index.html",
+        {
+            "upcoming_shows": us,
+            "show_all_events_link": True,
+            "show_home_link": False,
+            "newsletter_form": newsletter_form,
+        },
+    )
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
+
 
 def rentals(request):
-    return render(request, 'rentals.html',  {'rental_objects': _rental_objects()})
+    return render(request, "rentals.html", {"rental_objects": _rental_objects()})
+
 
 def international(request):
-    return render(request, 'international.html')
+    return render(request, "international.html")
+
 
 def event_list(request):
-    return render(request, 'events.html', {'upcoming_shows': _upcoming_shows(), 'show_home_link': True, 'show_all_events_link': False})
+    return render(
+        request,
+        "events.html",
+        {
+            "upcoming_shows": _upcoming_shows(),
+            "show_home_link": True,
+            "show_all_events_link": False,
+        },
+    )
+
 
 def newsletter_registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         n = NewsletterRegistrationForm(request.POST)
         if n.is_valid():
             n.save()
-            return render(request, 'newsletter_registered.html')
+            return render(request, "newsletter_registered.html")
 
-    return redirect('/')
+    return redirect("/")
+
 
 def handle404(request, exception):
-    return render(request, '404.html', status=404)
+    return render(request, "404.html", status=404)
 
 
 def server_error(request):
-    return render(request, 'error_message.html',
-                  {'error_code': 500, 'error_message': "Server Error"}, status=500)
+    return render(
+        request,
+        "error_message.html",
+        {"error_code": 500, "error_message": "Server Error"},
+        status=500,
+    )
 
 
 def permission_denied(request, exception):
-    return render(request, 'error_message.html',
-                  {'error_code': 403, 'error_message': "Permission Denied"}, status=403)
+    return render(
+        request,
+        "error_message.html",
+        {"error_code": 403, "error_message": "Permission Denied"},
+        status=403,
+    )
 
 
 def bad_request(request, exception):
-    return render(request, 'error_message.html',
-                  {'error_code': 400, 'error_message': "Bad Request"}, status=400)
+    return render(
+        request,
+        "error_message.html",
+        {"error_code": 400, "error_message": "Bad Request"},
+        status=400,
+    )
 
 
 def impressum(request):
-    ''' display impressum
-    '''
-    return render(request, 'impressum.html')
+    """display impressum"""
+    return render(request, "impressum.html")
+
 
 def datenschutz(request):
-    ''' display datenschutz
-    '''
-    return render(request, 'datenschutz.html')
+    """display datenschutz"""
+    return render(request, "datenschutz.html")
+
 
 def robots(request):
-    ''' display impressum
-    '''
+    """display impressum"""
     private_shows = Show.objects.filter(private=True)
     shows = Show.objects.filter(private=False)
-    return render(request, 'robots.txt', {'private_shows': private_shows})
+    return render(request, "robots.txt", {"private_shows": private_shows, "shows": shows})
 
 
 def sitemap(request):
-    ''' sitemap.xml
-    '''
+    """sitemap.xml"""
     shows = Show.objects.filter(private=False)
-    return render(request, 'sitemap.xml',
-                  {'shows': shows})
+    return render(request, "sitemap.xml", {"shows": shows})
