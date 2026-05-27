1c78c1871cd98d343a68c5cd922b384809e516e2
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2026-05-27 12:22:36 +0200

    chore: update imports after reservations reorganization

--- backend/events/models.py
+++ backend/events/models.py
@@ -67,7 +67,7 @@ class Event(models.Model):
 
         from django.db.models import Count
         from payments import PaymentStatus
-        from reservations.models import ReservationPayment
+        from reservations.payments.models import ReservationPayment
 
         result = ReservationPayment.objects.filter(
             reservation__event=self, status=PaymentStatus.CONFIRMED
