e4412d402087930854fb72d54293b23eaefd90c5
Philippe Ducasse <ducassephi@hotmail.fr>
2026-06-03 13:44:34 +0200

    remove custom paypal provider

--- backend/reservations/payments/urls.py
+++ backend/reservations/payments/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from reservations.payments import views
 
 urlpatterns = [
-    path("paypal-webhook/", views.paypal_webhook),
     path("<uuid:payment_id>/success", views.payment_success),
     path("<uuid:payment_id>/failure", views.payment_fail),
     path("<uuid:payment_id>", views.payment, name="payment"),
