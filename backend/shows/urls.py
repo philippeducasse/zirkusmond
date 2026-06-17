90cd20aa0e55b37d9eb43331b67f120330ecc93e
Philippe Ducasse <ducassephi@hotmail.fr>
2026-06-17 16:05:36 +0200

    feat: add show detail API endpoint

--- backend/shows/urls.py
+++ backend/shows/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from shows import views
 
 urlpatterns = [
+    # Template view (deprecated, will be removed after migration)
     path("show/<int:show_id>", views.show, name="show"),
+    # API endpoint
+    path("shows/<int:show_id>", views.get_show, name="show_detail"),
 ]
