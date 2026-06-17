3d4cffe1ec8740791007f91b7a6fa6cf9d46aae8
Philippe Ducasse <ducassephi@hotmail.fr>
2026-06-17 16:05:36 +0200

    feat: add show detail API endpoint

--- backend/shows/views.py
+++ backend/shows/views.py
@@ -1,5 +1,9 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from shows.serializers import ShowDetailSerializer
 
 from .models import Show
 
@@ -10,3 +14,10 @@ def show(request, show_id):
     except Show.DoesNotExist:
         raise Http404("Show does not exist")
     return render(request, "show.html", {"show": s})
+
+
+@api_view(["GET"])
+def get_show(request, show_id):
+    show = get_object_or_404(Show, id=show_id)
+
+    return Response({"show": ShowDetailSerializer(show).data})
