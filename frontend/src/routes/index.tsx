69fd0e9f004ca156aa4563a0acf77b79c7c080b2
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-03 21:36:40 +0200

    chore: migrated images and created dummy routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,17 @@
+import { Button } from '#/components/ui/button'
+import { Card } from '#/components/ui/card'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <h1>Index page</h1>
+  return (
+    <>
+      <h1>Index page</h1>
+      <Button variant={'outline'} size={'lg'}>
+        Button
+      </Button>
+      <Card></Card>
+    </>
+  )
 }
