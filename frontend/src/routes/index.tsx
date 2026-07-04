ebd0abed3e6e26273b6e339db6df93a1d4d87d80
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-04 21:41:13 +0200

    chore: revisting primary button styling

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,17 +1,12 @@
 import { Button } from '#/components/ui/button'
-import { Card } from '#/components/ui/card'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <>
-      <h1>Index page</h1>
-      <Button variant={'outline'} size={'lg'}>
-        Button
-      </Button>
-      <Card></Card>
-    </>
+    <div className="flex h-screen w-full">
+      <Button className="m-auto">Tickets Kaufen</Button>
+    </div>
   )
 }
