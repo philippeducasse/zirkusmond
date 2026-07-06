87f09fcb9f9cd8eeff895660ce72a33c3c96a066
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-06 22:09:08 +0200

    feat: add responsive navbar with locale switcher

---
+++ frontend/src/components/common/navbar/Logo.tsx
@@ -0,0 +1,14 @@
+import { Link } from '@tanstack/react-router'
+
+export default function Logo() {
+  return (
+    <Link to="/" className="flex justify-center">
+      <img
+        className="h-[56.25px] w-[56.25px] object-cover"
+        src="/images/logos/logo_16x9.webp"
+        alt="Zirkusmond logo"
+        width={100}
+      />
+    </Link>
+  )
+}
