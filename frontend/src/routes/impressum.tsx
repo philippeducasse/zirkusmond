c5b71b8784322ef070bbb97409dcd1d110879b98
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-07 21:54:39 +0200

    feat: add impressum and datenschutz legal pages

---
+++ frontend/src/routes/impressum.tsx
@@ -0,0 +1,43 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+import PageContainer from '#/components/common/PageContainer.tsx'
+
+export const Route = createFileRoute('/impressum')({
+  head: () => ({
+    meta: [{ title: 'Zirkus Mond - Impressum' }],
+  }),
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <PageContainer className="text-center lg:text-lg">
+      <h2 className="mb-5 text-2xl text-white">Impressum</h2>
+      <p>
+        Kultstätte 58 GmbH
+        <br />
+        Geschäftsführer Max Mohr
+        <br />
+        Körtestraße 38
+        <br />
+        10967 Berlin
+        <br />
+        <br />
+        Tel. Max Mohr: 015770280810
+        <br />
+        E-Mail: maxmohr@posteo.de
+        <br />
+        <br />
+        Steuer-Nr.: 37 / 406 / 50011
+        <br />
+        Steuer-ID: DE65 812 397 023
+        <br />
+        USt-IdNr: DE319431419
+        <br />
+        Finanzamt für Körperschaften II
+        <br />
+        Handelsregister-Nr.: HRB 164783B
+      </p>
+    </PageContainer>
+  )
+}
