2d8dc5fbe5facea2949bfc388172b71651a34bc9
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-07 21:54:37 +0200

    feat: wire about, events and contact pages to layout

--- frontend/src/routes/events.tsx
+++ frontend/src/routes/events.tsx
@@ -1,9 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import EventsSection from '#/components/common/home/EventsSection.tsx'
+import PageContainer from '#/components/common/PageContainer.tsx'
+
 export const Route = createFileRoute('/events')({
+  head: () => ({
+    meta: [
+      { title: 'Zirkus Mond – Shows & Events' },
+      {
+        name: 'description',
+        content:
+          'Alle Shows und Events im Zirkus Mond Berlin – Trapez, Akrobatik, Cabaret, Varieté, Physical Theatre und mehr. Jetzt Tickets sichern!',
+      },
+      {
+        name: 'keywords',
+        content:
+          'Zirkus Shows Berlin, Zirkus Events Berlin, Live Shows Berlin, Trapez, Akrobatik Berlin, Cabaret, Varieté, Physical Theatre, Familien Events Berlin',
+      },
+    ],
+  }),
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/events"!</div>
+  return (
+    <PageContainer>
+      <EventsSection showHomeLink />
+    </PageContainer>
+  )
 }
