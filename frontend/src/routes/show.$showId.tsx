57fe427519307e5f005de207cd7f77598403a789
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-08 14:28:34 +0200

    refactor: showDetails

--- frontend/src/routes/show.$showId.tsx
+++ frontend/src/routes/show.$showId.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, Link, notFound } from '@tanstack/react-router'
 
 import PageContainer from '#/components/common/PageContainer.tsx'
 import SectionDivider from '#/components/common/home/SectionDivider.tsx'
+import { ShowDetails } from '#/components/common/show/ShowDetails.tsx'
 import TimeDetails from '#/components/common/TimeDetails.tsx'
 import { Button } from '#/components/ui/button.tsx'
 import type { MockShow } from '#/lib/mock-shows.ts'
@@ -66,45 +67,7 @@ function RouteComponent() {
 
       <SectionDivider type="kite" />
 
-      <div className="flex flex-col text-lg lg:text-2xl">
-        <div>
-          <p className="mt-8 mb-4 text-3xl text-primary">About the show:</p>
-          <p>{show.description}</p>
-        </div>
-        <div className="mt-8 flex flex-wrap justify-center gap-12">
-          <img
-            src={show.cardImage}
-            alt={show.title}
-            className="aspect-square w-full max-w-sm object-cover"
-          />
-          <div className="flex flex-col lg:mt-12">
-            <p className="mt-4 mb-4 text-3xl text-primary">Cast:</p>
-            <p>{show.cast}</p>
-          </div>
-        </div>
-        <div className="mt-8 flex flex-wrap items-center justify-evenly gap-4">
-          {show.videoLink && (
-            <a
-              className="text-center text-2xl underline hover:text-white"
-              href={show.videoLink}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Trailer
-            </a>
-          )}
-          {show.websiteLink && (
-            <a
-              className="text-center text-2xl underline hover:text-white"
-              href={show.websiteLink}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Company's website
-            </a>
-          )}
-        </div>
-      </div>
+      <ShowDetails show={show} />
 
       <div className="my-12 flex justify-evenly">
         <Button asChild>
