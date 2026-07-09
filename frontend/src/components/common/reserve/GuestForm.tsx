611319cbfc5b3988953754c55ee4b23f95ba72f5
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-09 16:29:09 +0200

    wip: refactoring and restyling  reservation form

--- frontend/src/components/common/reserve/GuestForm.tsx
+++ frontend/src/components/common/reserve/GuestForm.tsx
@@ -1,4 +1,5 @@
-import FormField from '#/components/common/reserve/FormField.tsx'
+import DynamicForm from '#/components/common/reserve/DynamicForm.tsx'
+import { buildGuestFields } from '#/components/common/reserve/helper.ts'
 
 interface GuestFormProps {
   guestCount: number
@@ -10,23 +11,7 @@ export default function GuestForm({ guestCount }: GuestFormProps) {
   return (
     <div className="my-8 flex flex-col gap-6">
       {Array.from({ length: guestCount }, (_, i) => (
-        <div key={i} className="border border-primary/40 p-8">
-          <h4 className="mb-4 text-lg text-primary">Guest {i + 1}</h4>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 my-1">
-            <FormField
-              id={`guest-${i}-first-name`}
-              label="First name"
-              name={`guest-${i}-first-name`}
-              required
-            />
-            <FormField
-              id={`guest-${i}-last-name`}
-              label="Last name"
-              name={`guest-${i}-last-name`}
-              required
-            />
-          </div>
-        </div>
+        <DynamicForm fields={buildGuestFields(i)} title={`Guest ${i + 1}`} />
       ))}
     </div>
   )
