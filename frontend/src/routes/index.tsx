bb01e3595aeda9484b843ade2b19d9b7b7541b9d
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-13 21:20:13 +0200

    feat: fetch upcoming shows from API instead of mocks

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
 
 import EventsSection from '#/components/common/home/EventsSection.tsx'
 import FooterSection from '#/components/common/home/FooterSection.tsx'
 import GallerySection from '#/components/common/home/GallerySection.tsx'
 import Hero from '#/components/common/home/Hero.tsx'
+import { homepageQueryOptions } from '#/lib/api.ts'
 
 export const Route = createFileRoute('/')({
+  loader: ({ context: { queryClient } }) =>
+    queryClient.ensureQueryData(homepageQueryOptions),
   head: () => ({
     meta: [
       { title: 'Zirkus Mond' },
@@ -25,10 +29,12 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
+  const { data } = useSuspenseQuery(homepageQueryOptions)
+
   return (
     <>
       <Hero />
-      <EventsSection showAllEventsLink />
+      <EventsSection shows={data.upcoming_shows} showAllEventsLink />
       <GallerySection />
       <FooterSection />
     </>
