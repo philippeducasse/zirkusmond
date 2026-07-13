a931476959472589de714b8ffeb7fc92f5d206fb
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-13 21:20:00 +0200

    feat: wire TanStack Query SSR integration into router

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,13 +1,22 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import type { QueryClient } from '@tanstack/react-query'
 
 import { getLocale } from '#/paraglide/runtime'
 import Navbar from '#/components/common/Navbar.tsx'
 
 import appCss from '../styles.css?url'
 
-export const Route = createRootRoute({
+interface RouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
   beforeLoad: async () => {
     // Other redirect strategies are possible; see
     // https://github.com/TanStack/router/tree/main/examples/react/i18n-paraglide#offline-redirect
