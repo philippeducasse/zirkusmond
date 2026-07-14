3cae90707c1c8a71773345ba06e105698a8c838f
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-14 20:51:47 +0200

    refactor: reorganise reserve components into subfolder

--- frontend/src/routes/reserve.$showId.tsx
+++ frontend/src/routes/reserve.$showId.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, notFound } from '@tanstack/react-router'
 import { useSuspenseQuery } from '@tanstack/react-query'
 
 import PageContainer from '#/components/common/PageContainer.tsx'
-import ReservationPage from '#/components/common/reserve/reservationPage.tsx'
+import ReservationPage from '#/components/common/reserve/ReservationPage'
 import { ApiError, showQueryOptions } from '#/lib/api.ts'
 
 export const Route = createFileRoute('/reserve/$showId')({
