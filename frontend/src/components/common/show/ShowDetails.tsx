4e1517bf1d78c44a2a97ded8a302caaa977af2cf
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-15 20:48:10 +0200

    feat: render show description and cast as HTML

--- frontend/src/components/common/show/ShowDetails.tsx
+++ frontend/src/components/common/show/ShowDetails.tsx
@@ -9,7 +9,7 @@ export function ShowDetails({ show }: ShowDetailsProps) {
     <div className="flex flex-col text-base sm:text-lg lg:text-2xl">
       <div>
         <p className="mt-6 sm:mt-8 mb-3 sm:mb-4 text-xl sm:text-2xl md:text-3xl text-primary">About the show:</p>
-        <p>{show.description}</p>
+        <div dangerouslySetInnerHTML={{ __html: show.description }} />
       </div>
       <div className="mt-6 sm:mt-8 flex flex-wrap justify-center gap-8 sm:gap-12">
         <img
@@ -19,7 +19,7 @@ export function ShowDetails({ show }: ShowDetailsProps) {
         />
         <div className="flex flex-col lg:mt-12">
           <p className="mt-3 sm:mt-4 mb-3 sm:mb-4 text-xl sm:text-2xl md:text-3xl text-primary">Cast:</p>
-          <p>{show.cast}</p>
+          <div dangerouslySetInnerHTML={{ __html: show.cast }} />
         </div>
       </div>
       <div className="mt-6 sm:mt-8 flex flex-wrap items-center justify-evenly gap-4">
