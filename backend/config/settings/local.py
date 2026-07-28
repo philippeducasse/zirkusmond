d848c5b1d4c66908acead407a0d13be274259be9
Philippe Ducasse <ducassephi@hotmail.fr>
2026-07-28 15:35:41 +0200

    feat: add CORS support for frontend

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -5,8 +5,14 @@ from .base import *
 DEBUG = True
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000", "http://localhost:3000"]
 FRONTEND_URL = "http://localhost:3000"
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 PAYMENT_HOST = "localhost:8000"
 PAYMENT_USES_SSL = False
