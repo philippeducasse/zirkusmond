6289d6d695708218151c341e0eceff97bbbc7945
Philippe Ducasse <ducassephi@hotmail.fr>
2026-08-16 16:31:24 +0200

    style: widen footer and use paragraphs for legal text

--- frontend/src/components/zirkusmond/general/Footer.tsx
+++ frontend/src/components/zirkusmond/general/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <>
       <SectionDivider type="moon" margin={'small'} />
-      <div className="max-w-2/3 mx-auto flex flex-col text-center p-1 md:p-8 gap-6">
+      <div className="w-full md:max-w-2/3 mx-auto flex flex-col text-center p-1 md:p-8 gap-6">
         <div className="flex gap-6 justify-center text-white ">
           <Link
             to="/impressum"
@@ -22,15 +22,15 @@ const Footer = () => {
           </Link>
         </div>
         <div className="flex flex-col md:flex-row align-middle justify-centertext-sm sm:text-base">
-          <div className="px-4 lg:w-1/3">
+          <p className="px-4 lg:w-1/3 text-sm">
             Lilli-Henoch-Str. Office at: Kultstätte 58 GmbH Körtestraße 38 10967
             Berlin
-          </div>
-          <div className="px-4 lg:w-1/3">Geschäftsführer Max Mohr</div>
-          <div className="px-4 lg:w-1/3">
+          </p>
+          <p className="px-4 lg:w-1/3 text-sm">Geschäftsführer Max Mohr</p>
+          <p className="px-4 lg:w-1/3 text-sm">
             Steuer-Nr.: 37 / 406 / 50011 Steuer-ID: DE65 812 397 023 USt-IdNr:
             DE319431419
-          </div>
+          </p>
         </div>
       </div>
     </>
