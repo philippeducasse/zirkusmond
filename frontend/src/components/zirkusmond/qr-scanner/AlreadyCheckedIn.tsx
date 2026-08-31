3af79ff8b4b6496b226dd002f47ff295d58df66d
Philippe Ducasse <ducassephi@hotmail.fr>
2026-08-31 12:51:07 +0200

    fix: text color on scanner

--- frontend/src/components/zirkusmond/qr-scanner/AlreadyCheckedIn.tsx
+++ frontend/src/components/zirkusmond/qr-scanner/AlreadyCheckedIn.tsx
@@ -7,9 +7,7 @@ interface AlreadyCheckedInProps {
 export const AlreadyCheckedIn = ({ data }: AlreadyCheckedInProps) => {
   return (
     <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-6 text-center">
-      <h2 className="font-bold text-yellow-800 text-6xl mb-2">
-        Already Checked In
-      </h2>
+      <h2 className="font-bold text-yellow-800 text-6xl mb-2">Already Checked In</h2>
       <div className="flex flex-col items-center">
         <svg
           className="w-28 h-28 mx-auto text-yellow-500 mb-4"
@@ -25,10 +23,10 @@ export const AlreadyCheckedIn = ({ data }: AlreadyCheckedInProps) => {
           />
         </svg>
         <div className="text-left">
-          <p className="text-5xl">
+          <p className="text-5xl text-black">
             <strong>Reservation ID:</strong> {data.reservationNumber}
           </p>
-          <p className="text-5xl">
+          <p className="text-5xl text-black">
             <strong>Guests:</strong> {data.guests.join(", ")}
           </p>
         </div>
