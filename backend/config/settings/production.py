d79fb7525f1406157227efddffe0d25c617b968b
Philippe Ducasse <ducassephi@hotmail.fr>
2026-08-31 12:15:28 +0200

    fixing cors header on qr code scanner

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 DEBUG = False
@@ -7,6 +9,19 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
 ]
+
+CSRF_TRUSTED_ORIGINS = ["https://zirkusmond.de", "https://www.zirkusmond.de"]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://zirkusmond.de",
+    "https://www.zirkusmond.de",
+]
+CORS_ALLOW_CREDENTIALS = True
+
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = "Lax"
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = "Lax"
 
 PAYMENT_USES_SSL = True
 FRONTEND_URL = os.environ.get("FRONTEND_URL", "https://zirkusmond.de")
