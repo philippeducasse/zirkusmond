bf41d7bbcd7c96bd708bf2117ada6e9e60f9bbf6
Philippe Ducasse <ducassephi@hotmail.fr>
2026-09-10 14:57:19 +0200

    fix: fixed deployment for celery

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -142,7 +142,7 @@ CELERY_TIMEZONE = TIME_ZONE
 CELERY_BEAT_SCHEDULE = {
     "cleanup-abandoned-payments": {
         "task": "reservations.tasks.cleanup_abandoned_payments",
-        "schedule": 3600.0,  # 6 hours
+        "schedule": 3600.0,  # 1 hours
     },
 }
 
