4d67c8d12e6d2fcbccdc921d222a8159c3dee7a7
Philippe Ducasse <ducassephi@hotmail.fr>
2026-09-12 16:06:52 +0200

    fix: remove debug toolbar from staging

--- backend/config/settings/staging.py
+++ backend/config/settings/staging.py
@@ -8,5 +8,3 @@ ALLOWED_HOSTS = ["testing.zirkusmond.de", "localhost"]
 PAYMENT_USES_SSL = True
 FRONTEND_URL = os.environ.get("FRONTEND_URL", "https://testing.zirkusmond.de")
 PAYMENT_HOST = "testing.zirkusmond.de"
-INSTALLED_APPS += ["debug_toolbar"]
-MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
